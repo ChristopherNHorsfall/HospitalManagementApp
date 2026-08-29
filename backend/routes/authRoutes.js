8a637f551c3808f3cb8dae02e5b4381bc33ecd0c
ChristopherNHorsfall <113425702+ChristopherNHorsfall@users.noreply.github.com>
2026-08-29 19:31:14 +1000

    Story3 Role based access, HMA-23 3.2 implement role authorisation

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,6 +3,7 @@ const { login } = require("../controllers/authController");
 
 const router = express.Router();
 const protect = require("../middleware/authMiddleware"); // temporary
+const authorizeRoles = require("../middleware/roleMiddleware");
 
 router.post("/login", login);
 router.post("/login", login);
@@ -13,5 +14,17 @@ router.get("/profile", protect, (req, res) => {
         user: req.user,
     });
 });
+/*
+router.get("/doctor-test", protect, authorizeRoles("doctor"), (req, res) => {
+    res.json({
+        message: "Doctor access granted",
+    });
+});
 
+router.get("/nurse-test", protect, authorizeRoles("nurse"), (req, res) => {
+    res.json({
+        message: "Nurse access granted",
+    });
+});
+*/
 module.exports = router;
